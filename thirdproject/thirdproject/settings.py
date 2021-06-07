024800887a20324ead2c54eea2228dc18f58925d
poojithabalaji <85452879+poojithabalaji@users.noreply.github.com>
2021-06-07 17:29:50 +0530

    Update settings.py

--- thirdproject/thirdproject/settings.py
+++ thirdproject/thirdproject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-p!%o3mc$s*1^+c==vejxl^==6mhuxh1m^@7(76@u(#6jrmbqfm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.pythonanywhere.com']
 
 
 # Application definition
